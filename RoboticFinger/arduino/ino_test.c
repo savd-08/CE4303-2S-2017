354bd7693beea241025a2ef66e0b79f485c326fd
Brais <abcbrais@gmail.com>
2017-11-10 23:31:35 -0600

    se agrego una prueba del struct

---
+++ RoboticFinger/arduino/ino_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+FILE * device;
+
+/* Structures, global variables */
+typedef struct point {
+  uint8_t x;
+  uint8_t y;
+  uint8_t z;
+} point_struct;
+
+int main(int argc, char const *argv[]) {
+  device = fopen("/dev/arduino2","r+");
+
+  point_struct point_s;
+
+  point_s.x = 40;
+  point_s.y = 40;
+  point_s.z = 0;
+
+  if(device != NULL){
+
+      fwrite(&point_s, sizeof(struct point), 1, device);
+
+      fclose(device);
+  }
+
+  return 0;
+}
